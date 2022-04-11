c7fe4a2458f6831bbe03e3aa46a3c85c13877ad3
Cole Voss <voss.cole@gmail.com>
2022-04-11 14:55:13 -0500

    better starup logging

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 8080;
 
 async function main() {
   try {
+    app.log.info({ port: PORT }, 'Starting application');
     const address = await app.listen(PORT, '0.0.0.0');
-    console.log(`App listening at ${address}`);
+    app.log.info({ address }, 'App started');
     console.log(app.printRoutes());
   } catch (err) {
     console.error(err);
